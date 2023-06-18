e74d4b11ea645317e2bd445b35625565e6be8398
mitevans <mitchell.evans27@gmail.com>
2023-06-18 10:53:34 -0700

    Added billing information overrides for drop_shippers

--- create_sales_order/main.py
+++ create_sales_order/main.py
@@ -1,4 +1,5 @@
 from input_data import *
+
 import urllib3
 import certifi
 import json
@@ -8,9 +9,7 @@ import ast
 
 def create_new_sales_order(api_body, request_access_token):
     time.sleep(1)
-    print("create_new_sales_order api_body: ", api_body)
     encoded_body = json.dumps(api_body)
-    print("create_new_sales_order body: ", encoded_body)
     resp = http.request(
         "POST",
         "https://api.sosinventory.com/api/v2/salesorder/",
@@ -20,18 +19,13 @@ def create_new_sales_order(api_body, request_access_token):
         },
         body=encoded_body
     )
-    print("create_new_sales_order return:", resp)
     json_object = json.loads(resp.data.decode("utf-8"))
-    print("create_new_sales_order json return:")
-    print(json_object)
     return json_object
 
 
 def clean_purchased_products_list(item_purchased_products_list):
     item_purchased_products_list = "[" +item_purchased_products_list + "]"
-    print("purchased_products_list: ",item_purchased_products_list)
     item_purchased_products_list = json.loads(item_purchased_products_list)
-    print("purchased_products_list after: ",item_purchased_products_list)
     return item_purchased_products_list
 
 
@@ -48,17 +42,39 @@ payment_names = input_data['payment_names']
 customer_default_address_province_code = input_data['customer_default_address_province_code']
 customer_default_address_phone = input_data['customer_default_address_phone']
 email = input_data['email']
-customer_default_address_address1 = input_data['customer_default_address_address1']
-customer_default_address_address2 = input_data['customer_default_address_address2']
-if customer_default_address_address2 == "null" or customer_default_address_address2 == "" or customer_default_address_address2 == '\"\"':
-    print("customer_default_address_address2 is null or empty")
-    customer_default_address_address2 = None
+customer_default_address_line1 = input_data['customer_default_address_line1']
+customer_default_address_line2 = input_data['customer_default_address_line2']
+if customer_default_address_line2 == "null" or customer_default_address_line2 == "" or customer_default_address_line2 == '\"\"':
+    print("customer_default_address_line2 is null or empty")
+    customer_default_address_line2 = None
 customer_default_address_city = input_data['customer_default_address_city']
 customer_default_address_zip = input_data['customer_default_address_zip']
 customer_default_address_country_code = input_data['customer_default_address_country_code']
 total_shipping_price_set_shop_money_amount = input_data['total_shipping_price_set_shop_money_amount']
 purchased_products_list = clean_purchased_products_list(input_data['purchased_products_list'])
-print("purchased_products_list:",  purchased_products_list)
+is_drop_shipper = input_data['is_drop_shipper']
+if is_drop_shipper == "true":
+    billing_customer_default_address_name = input_data['billing_customer_default_address_name']
+    billing_customer_default_address_phone = input_data['billing_customer_default_address_phone']
+    billing_email = input_data['billing_email']
+    billing_customer_default_address_line1 = input_data['billing_customer_default_address_line1']
+    billing_customer_default_address_line2 = input_data['billing_customer_default_address_line2']
+    if billing_customer_default_address_line2 == "null" or billing_customer_default_address_line2 == "" or billing_customer_default_address_line2 == '\"\"':
+        billing_customer_default_address_line2 = None
+    billing_customer_default_address_city = input_data['billing_customer_default_address_city']
+    billing_customer_default_address_province_code = input_data['billing_customer_default_address_province_code']
+    billing_customer_default_address_zip = input_data['billing_customer_default_address_zip']
+    billing_customer_default_address_country_code = input_data['billing_customer_default_address_country_code']
+else:
+    billing_customer_default_address_name = customer_default_address_name
+    billing_customer_default_address_phone = customer_default_address_phone
+    billing_email = email
+    billing_customer_default_address_line1 = customer_default_address_line1
+    billing_customer_default_address_line2 = customer_default_address_line2
+    billing_customer_default_address_city = customer_default_address_city
+    billing_customer_default_address_province_code = customer_default_address_province_code
+    billing_customer_default_address_zip = customer_default_address_zip
+    billing_customer_default_address_country_code = customer_default_address_country_code
 
 
 
@@ -70,127 +86,150 @@ http = urllib3.PoolManager(
     timeout=urllib3.Timeout(connect=connect_timeout, read=read_timeout)
 )
 
-body = {
-    "starred": 0,
-    "syncToken": 0,
-    "number": "auto",
-    "date": updated_at,
-    "customer": {
-        "name": customer_default_address_name
-    },
-    "location": {
-        "id": 1,
-        "name": ""
-    },
-    "billing": {
-        "company": None,
-        "contact": customer_default_address_name,
-        "phone": customer_default_address_phone,
-        "email": email,
-        "addressName": "",
-        "addressType": "",
-        "address": {
-            "line1": customer_default_address_address1,
-            "line2": customer_default_address_address2,
-            "line3": None,
-            "line4": None,
-            "line5": None,
-            "city": customer_default_address_city,
-            "stateProvince": customer_default_address_province_code,
-            "postalCode": customer_default_address_zip,
-            "country": customer_default_address_country_code
-        }
-    },
-    "shipping": {
-        "company": None,
-        "contact": customer_default_address_name,
-        "phone": customer_default_address_phone,
-        "email": email,
-        "addressName": "",
-        "addressType": "",
-        "address": {
-            "line1": customer_default_address_address1,
-            "line2": customer_default_address_address2,
-            "line3": None,
-            "line4": None,
-            "line5": None,
-            "city": customer_default_address_city,
-            "stateProvince": customer_default_address_province_code,
-            "postalCode": customer_default_address_zip,
-            "country": customer_default_address_country_code
-        }
-    },
-    "terms": {
-        "name": payment_names
-    },
-    "salesRep": None,
-    "channel": None,
-    "department": None,
-    "priority": None,
-    "assignedToUser": None,
-    "orderStage": {
-        "id": 1,
-        "name": "Processing"
-    },
-    "currency": {
-        "id": 42,
-        "name": "USD"
-    },
-    "serial": None,
-    "linkedTransaction": None,
-    "linkedInvoices": [
+def create_body(name, updated_at, payment_names, is_drop_shipper, total_shipping_price_set_shop_money_amount,
+                purchased_products_list, taxable, tax_percent, calculated_total, formatted_discount_text,  subtotal_price,
+                customer_default_address_name, billing_customer_default_address_name,
+                customer_default_address_phone, billing_customer_default_address_phone,
+                email, billing_email,
+                customer_default_address_line1, billing_customer_default_address_line1,
+                customer_default_address_line2, billing_customer_default_address_line2,
+                customer_default_address_city, billing_customer_default_address_city,
+                customer_default_address_province_code, billing_customer_default_address_province_code,
+                customer_default_address_zip, billing_customer_default_address_zip,
+                customer_default_address_country_code, billing_customer_default_address_country_code):
+    body = {
+        "starred": 0,
+        "syncToken": 0,
+        "number": "auto",
+        "date": updated_at,
+        "customer": {
+            "name": billing_customer_default_address_name
+        },
+        "location": {
+            "id": 1,
+            "name": ""
+        },
+        "billing": {
+            "company": None,
+            "contact": billing_customer_default_address_name,
+            "phone": billing_customer_default_address_phone,
+            "email": billing_email,
+            "addressName": "",
+            "addressType": "",
+            "address": {
+                "line1": billing_customer_default_address_line1,
+                "line2": billing_customer_default_address_line2,
+                "line3": None,
+                "line4": None,
+                "line5": None,
+                "city": billing_customer_default_address_city,
+                "stateProvince": billing_customer_default_address_province_code,
+                "postalCode": billing_customer_default_address_zip,
+                "country": billing_customer_default_address_country_code
+            }
+        },
+        "shipping": {
+            "company": None,
+            "contact": customer_default_address_name,
+            "phone": customer_default_address_phone,
+            "email": email,
+            "addressName": "",
+            "addressType": "",
+            "address": {
+                "line1": customer_default_address_line1,
+                "line2": customer_default_address_line2,
+                "line3": None,
+                "line4": None,
+                "line5": None,
+                "city": customer_default_address_city,
+                "stateProvince": customer_default_address_province_code,
+                "postalCode": customer_default_address_zip,
+                "country": customer_default_address_country_code
+            }
+        },
+        "terms": {
+            "name": payment_names
+        },
+        "salesRep": None,
+        "channel": None,
+        "department": None,
+        "priority": None,
+        "assignedToUser": None,
+        "orderStage": {
+            "id": 1,
+            "name": "Processing"
+        },
+        "currency": {
+            "id": 42,
+            "name": "USD"
+        },
+        "serial": None,
+        "linkedTransaction": None,
+        "linkedInvoices": [
 
-    ],
-    "linkedShipments": [
-    ],
-    "exchangeRate": 1.0,
-    "customerMessage": None,
-    "statusMessage": None,
-    "comment": "Porcupine: ," + customer_default_address_name + " PO#: " + name,
-    "customerNotes": "",
-    "customFields": [
-        {
-            "id": 14,
-            "name": "In Stock Date",
-            "value": None,
-            "dataType": "Text"
-        },
-        {
-            "id": 12,
-            "name": "Manufacturing Date",
-            "value": None,
-            "dataType": "Date"
-        },
-        {
-            "id": 13,
-            "name": "SO Printed",
-            "value": False,
-            "dataType": "Boolean"
-        }
-    ],
-    "customerPO": name,
-    "depositPercent": 0.00000,
-    "depositAmount": 0.00000,
-    "subTotal": subtotal_price,
-    "discountPercent": 0.00000,
-    "discountAmount": formatted_discount_text,
-    "taxPercent": tax_percent,
-    "taxAmount": 0.00,
-    "shippingAmount": total_shipping_price_set_shop_money_amount,
-    "total": calculated_total,
-    "discountTaxable": taxable,
-    "shippingTaxable": False,
-    "dropShip": False,
-    "closed": False,
-    "archived": False,
-    "summaryOnly": False,
-    "hasSignature": False,
-    "keys": None,
-    "values": None,
-    "lines": purchased_products_list
-}
+        ],
+        "linkedShipments": [
+        ],
+        "exchangeRate": 1.0,
+        "customerMessage": None,
+        "statusMessage": None,
+        "comment": "Porcupine: " + customer_default_address_name + ", PO#: " + name,
+        "customerNotes": "",
+        "customFields": [
+            {
+                "id": 14,
+                "name": "In Stock Date",
+                "value": None,
+                "dataType": "Text"
+            },
+            {
+                "id": 12,
+                "name": "Manufacturing Date",
+                "value": None,
+                "dataType": "Date"
+            },
+            {
+                "id": 13,
+                "name": "SO Printed",
+                "value": False,
+                "dataType": "Boolean"
+            }
+        ],
+        "customerPO": name,
+        "depositPercent": 0.00000,
+        "depositAmount": 0.00000,
+        "subTotal": subtotal_price,
+        "discountPercent": 0.00000,
+        "discountAmount": formatted_discount_text,
+        "taxPercent": tax_percent,
+        "taxAmount": 0.00,
+        "shippingAmount": total_shipping_price_set_shop_money_amount,
+        "total": calculated_total,
+        "discountTaxable": taxable,
+        "shippingTaxable": False,
+        "dropShip": False,
+        "closed": False,
+        "archived": False,
+        "summaryOnly": False,
+        "hasSignature": False,
+        "keys": None,
+        "values": None,
+        "lines": purchased_products_list
+    }
+    return body
+
+
+body = create_body(name, updated_at, payment_names, is_drop_shipper, total_shipping_price_set_shop_money_amount,
+                purchased_products_list, taxable, tax_percent, calculated_total, formatted_discount_text,  subtotal_price,
+                customer_default_address_name, billing_customer_default_address_name,
+                customer_default_address_phone, billing_customer_default_address_phone,
+                email, billing_email,
+                customer_default_address_line1, billing_customer_default_address_line1,
+                customer_default_address_line2, billing_customer_default_address_line2,
+                customer_default_address_city, billing_customer_default_address_city,
+                customer_default_address_province_code, billing_customer_default_address_province_code,
+                customer_default_address_zip, billing_customer_default_address_zip,
+                customer_default_address_country_code, billing_customer_default_address_country_code)
 print("body:", body)
-print ("purchased_products_list", purchased_products_list)
-# create_sales_order_json = create_new_sales_order(body,access_token)
-# print(create_sales_order_json)
-# output = [{'create_sales_order_json': create_sales_order_json}]
+create_sales_order_json = create_new_sales_order(body,access_token)
+output = [{'create_sales_order_json': create_sales_order_json}]
--- create_sales_order/test.py
+++ create_sales_order/test.py
@@ -3,7 +3,20 @@ from main import *
 
 def test_clean_purchased_products_list_multiple_line_items():
     test_purchased_products_list_multiple_line_items = '{"lineNumber": 1, "item": {"name": "MESO Customs:CLIMATE-KNOBS-DZ-BLACK"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": true, "taxCode": null}, "linkedTransaction": null, "description": "Climate Control Rings // DUAL ZONE // BLACK", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 30.0, "amount": 30.0, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "60.00", "duedate": "2023-06-22T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null},{"lineNumber": 2, "item": {"name": "3GT-VENT-BLACK"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": true, "taxCode": null}, "linkedTransaction": null, "description": "3GT-VENT-BLACK", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 30.0, "amount": 30.0, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "60.00", "duedate": "2023-06-22T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null}'
-    expected = [{'lineNumber': 1, 'item': {'name': 'MESO Customs:CLIMATE-KNOBS-DZ-BLACK'}, 'class': {'id': 1, 'name': 'Ecoological'}, 'job': None, 'workcenter': None, 'tax': {'taxable': True, 'taxCode': None}, 'linkedTransaction': None, 'description': 'Climate Control Rings // DUAL ZONE // BLACK', 'quantity': 1, 'weight': 0.0, 'volume': 0.0, 'weightunit': 'lb', 'volumeunit': 'cbm', 'percentdiscount': 0, 'unitprice': 30.0, 'amount': 30.0, 'altAmount': 0.0, 'picked': 0.0, 'shipped': 0.0, 'returned': 0.0, 'cost': None, 'margin': None, 'listPrice': '60.00', 'duedate': '2023-06-22T00:00:00', 'uom': None, 'bin': None, 'lot': None, 'serials': None}, {'lineNumber': 2, 'item': {'name': '3GT-VENT-BLACK'}, 'class': {'id': 1, 'name': 'Ecoological'}, 'job': None, 'workcenter': None, 'tax': {'taxable': True, 'taxCode': None}, 'linkedTransaction': None, 'description': '3GT-VENT-BLACK', 'quantity': 1, 'weight': 0.0, 'volume': 0.0, 'weightunit': 'lb', 'volumeunit': 'cbm', 'percentdiscount': 0, 'unitprice': 30.0, 'amount': 30.0, 'altAmount': 0.0, 'picked': 0.0, 'shipped': 0.0, 'returned': 0.0, 'cost': None, 'margin': None, 'listPrice': '60.00', 'duedate': '2023-06-22T00:00:00', 'uom': None, 'bin': None, 'lot': None, 'serials': None}]
+    expected = [{'lineNumber': 1, 'item': {'name': 'MESO Customs:CLIMATE-KNOBS-DZ-BLACK'},
+                 'class': {'id': 1, 'name': 'Ecoological'}, 'job': None, 'workcenter': None,
+                 'tax': {'taxable': True, 'taxCode': None}, 'linkedTransaction': None,
+                 'description': 'Climate Control Rings // DUAL ZONE // BLACK', 'quantity': 1, 'weight': 0.0,
+                 'volume': 0.0, 'weightunit': 'lb', 'volumeunit': 'cbm', 'percentdiscount': 0, 'unitprice': 30.0,
+                 'amount': 30.0, 'altAmount': 0.0, 'picked': 0.0, 'shipped': 0.0, 'returned': 0.0, 'cost': None,
+                 'margin': None, 'listPrice': '60.00', 'duedate': '2023-06-22T00:00:00', 'uom': None, 'bin': None,
+                 'lot': None, 'serials': None},
+                {'lineNumber': 2, 'item': {'name': '3GT-VENT-BLACK'}, 'class': {'id': 1, 'name': 'Ecoological'},
+                 'job': None, 'workcenter': None, 'tax': {'taxable': True, 'taxCode': None}, 'linkedTransaction': None,
+                 'description': '3GT-VENT-BLACK', 'quantity': 1, 'weight': 0.0, 'volume': 0.0, 'weightunit': 'lb',
+                 'volumeunit': 'cbm', 'percentdiscount': 0, 'unitprice': 30.0, 'amount': 30.0, 'altAmount': 0.0,
+                 'picked': 0.0, 'shipped': 0.0, 'returned': 0.0, 'cost': None, 'margin': None, 'listPrice': '60.00',
+                 'duedate': '2023-06-22T00:00:00', 'uom': None, 'bin': None, 'lot': None, 'serials': None}]
     print("expected: ", expected)
     actual = clean_purchased_products_list(test_purchased_products_list_multiple_line_items)
     print("actual: ", actual)
@@ -11,12 +24,53 @@ def test_clean_purchased_products_list_multiple_line_items():
 
 
 def test_clean_purchased_products_list_single_line_item():
-    test_purchased_products_list_single_line_item = '{"lineNumber": 1, "item": {"name": "BumperShellz:EK4001"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": false, "taxCode": null}, "linkedTransaction": null, "description": "Make: CHEVY Model: SILVERADO 1500 Year: \'19-\'23 Location: REAR BUMPER Color: GLOSS BLACK Alt1: WITH Sensor Holes Alt2: DUAL EXHAUST # of Pieces: 3", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 204.95, "amount": 204.95, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "30.0", "duedate": "2023-05-21T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null}'
-    expected = [{'lineNumber': 1, 'item': {'name': 'BumperShellz:EK4001'}, 'class': {'id': 1, 'name': 'Ecoological'}, 'job': None, 'workcenter': None, 'tax': {'taxable': False, 'taxCode': None}, 'linkedTransaction': None, 'description': 'Make: CHEVY Model: SILVERADO 1500 Year: 19-23 Location: REAR BUMPER Color: GLOSS BLACK Alt1: WITH Sensor Holes Alt2: DUAL EXHAUST # of Pieces: 3', 'quantity': 1, 'weight': 0.0, 'volume': 0.0, 'weightunit': 'lb', 'volumeunit': 'cbm', 'percentdiscount': 0, 'unitprice': 204.95, 'amount': 204.95, 'altAmount': 0.0, 'picked': 0.0, 'shipped': 0.0, 'returned': 0.0, 'cost': None, 'margin': None, 'listPrice': '30.0', 'duedate': '2023-05-21T00:00:00', 'uom': None, 'bin': None, 'lot': None, 'serials': None}]
+    test_purchased_products_list_single_line_item = '{"lineNumber": 1, "item": {"name": "BumperShellz:EK4001"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": false, "taxCode": null}, "linkedTransaction": null, "description": "Make: CHEVY Model: SILVERADO 1500 Year: 19-23", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 204.95, "amount": 204.95, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "30.0", "duedate": "2023-05-21T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null}'
+    expected = [{'lineNumber': 1, 'item': {'name': 'BumperShellz:EK4001'}, 'class': {'id': 1, 'name': 'Ecoological'},
+                 'job': None, 'workcenter': None, 'tax': {'taxable': False, 'taxCode': None}, 'linkedTransaction': None,
+                 'description': 'Make: CHEVY Model: SILVERADO 1500 Year: 19-23',
+                 'quantity': 1, 'weight': 0.0, 'volume': 0.0, 'weightunit': 'lb', 'volumeunit': 'cbm',
+                 'percentdiscount': 0, 'unitprice': 204.95, 'amount': 204.95, 'altAmount': 0.0, 'picked': 0.0,
+                 'shipped': 0.0, 'returned': 0.0, 'cost': None, 'margin': None, 'listPrice': '30.0',
+                 'duedate': '2023-05-21T00:00:00', 'uom': None, 'bin': None, 'lot': None, 'serials': None}]
     print("expected: ", expected)
     actual = clean_purchased_products_list(test_purchased_products_list_single_line_item)
     print('actual:', actual)
     assert (actual == expected)
 
 
+def test_create_body_not_drop_shipper():
+    test_body = create_body('mitchell-test-CS2122', '2022-05-11T08:28:55-04:00', 'Due on receipt', 'true', '8',
+                            '{"lineNumber": 1, "item": {"name": "BumperShellz:EK4001"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": false, "taxCode": null}, "linkedTransaction": null, "description": "Make: CHEVY Model: SILVERADO 1500 Year: \'19-\'23 Location: REAR BUMPER Color: GLOSS BLACK Alt1: WITH Sensor Holes Alt2: DUAL EXHAUST # of Pieces: 3", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 204.95, "amount": 204.95, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "30.0", "duedate": "2023-05-21T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null}',
+                            'false', '0', '38', '0.00', '30',
+                            'John Doe', 'John Doe',
+                            '7273959261', '7273959261',
+                            'customer@gmail.com', 'customer@gmail.com',
+                            '14917 1st st e', '1234 turn5 dr',
+                            '\"\"', '\"\"',
+                            'Maderia Beach', 'Maderia Beach',
+                            'FL', 'FL',
+                            '33708', '33708',
+                            'US', 'US')
+    assert (test_body['customer']['name'] == 'John Doe')
+    assert (test_body['date'] == '2022-05-11T08:28:55-04:00')
+    assert (test_body['billing']['contact'] == 'John Doe')
+    assert (test_body['customerPO'] == 'mitchell-test-CS2122')
+
+def test_create_body_dropshipper():
+    test_body = create_body('mitchell-test-CS2122', '2022-05-11T08:28:55-04:00', 'Due on receipt', 'true', '8',
+                            '{"lineNumber": 1, "item": {"name": "BumperShellz:EK4001"}, "class": {"id": 1, "name": "Ecoological"}, "job": null, "workcenter": null, "tax": {"taxable": false, "taxCode": null}, "linkedTransaction": null, "description": "Make: CHEVY Model: SILVERADO 1500 Year: \'19-\'23 Location: REAR BUMPER Color: GLOSS BLACK Alt1: WITH Sensor Holes Alt2: DUAL EXHAUST # of Pieces: 3", "quantity": 1, "weight": 0.0, "volume": 0.0, "weightunit": "lb", "volumeunit": "cbm", "percentdiscount": 0, "unitprice": 204.95, "amount": 204.95, "altAmount": 0.0, "picked": 0.0, "shipped": 0.0, "returned": 0.0, "cost": null, "margin": null, "listPrice": "30.0", "duedate": "2023-05-21T00:00:00", "uom": null, "bin": null, "lot": null, "serials": null}',
+                            'false', '0', '38', '0.00',  '30',
+                            'John Doe', 'Turn5',
+                            '7273959261', '1234567891',
+                            'customer@gmail.com', 'turn5@gmail.com',
+                            '14917 1st st e', '1234 turn5 dr',
+                            '\"\"', '\"\"',
+                            'Maderia Beach', 'Turn5 City',
+                            'FL', 'GA',
+                            '33708', '12345',
+                            'US', 'US')
+    assert (test_body['customer']['name'] == 'Turn5')
+    assert (test_body['date'] == '2022-05-11T08:28:55-04:00')
+    assert (test_body['billing']['contact'] == 'Turn5')
+    assert (test_body['customerPO'] == 'mitchell-test-CS2122')
 
