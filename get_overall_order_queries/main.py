7df49eda16f5fc1e1b354dc71fc8a9a4b7fbf2b4
mitevans <mitchell.evans27@gmail.com>
2023-06-20 22:47:22 -0700

    Normalizing the input/output to match zapier format

--- get_overall_order_queries/main.py
+++ get_overall_order_queries/main.py
@@ -1,5 +1,11 @@
 from input_data import *
 
+total_price = input_data['total_price']
+subtotal_price = input_data['subtotal_price']
+total_discounts = input_data['total_discounts']
+state = input_data['state']
+total_tax = input_data['total_tax']
+
 
 def get_taxable(code_state):
     if code_state == "CA":
@@ -31,10 +37,10 @@ def get_formatted_discount_text(discounts):
 
 
 taxable = get_taxable(state)
-print(taxable)
 calculated_total = float(total_price) - float(total_discounts)
-print(calculated_total)
-tax_percent = get_tax_percent(total_tax, taxable, subtotal_price, total_discounts)
-print(tax_percent)
-formatted_discount_text = get_formatted_discount_text(total_discounts)
-print(formatted_discount_text)
+tax_percent = get_tax_percent(float(total_tax), taxable, float(subtotal_price), float(total_discounts))
+formatted_discount_text = get_formatted_discount_text(float(total_discounts))
+
+output = [{'taxable': taxable, 'calculated_total': calculated_total, 'tax_percent': tax_percent,
+           'formatted_discount_text': formatted_discount_text}]
+print(output)
